98602aacca140705db0d1b90cdbcc6a7165bf108
james-julius <jamesdarby7@gmail.com>
2024-04-06 16:31:37 -0700

    feat: add harry potter memory events

--- components/memory-display.tsx
+++ components/memory-display.tsx
@@ -21,129 +21,198 @@ interface Memory {
 
 const memories: Memory[] = [
   {
-    event: 'First day of school',
-    date: '2021-09-01',
+    event: 'Dudley’s Birthday at the Zoo',
+    date: '1991-06-23',
     emotions: {
-      fear: 7,
-      anger: 2,
-      sadness: 6,
-      surprise: 4,
-      joy: 3,
+      fear: 3,
+      anger: 4,
+      sadness: 5,
+      surprise: 7,
+      joy: 2,
       love: 1
     }
   },
   {
-    event: 'Summer Vacation at the Beach',
-    date: '2021-09-01',
+    event: 'Meeting Rubeus Hagrid and Learning He’s a Wizard',
+    date: '1991-07-31',
     emotions: {
-      fear: 6,
+      fear: 4,
+      anger: 1,
+      sadness: 2,
+      surprise: 10,
+      joy: 9,
+      love: 3
+    }
+  },
+  {
+    event: 'Meeting Ron Weasley',
+    date: '1991-09-01',
+    emotions: {
+      fear: 2,
+      anger: 1,
+      sadness: 1,
+      surprise: 3,
+      joy: 8,
+      love: 6
+    }
+  },
+  {
+    event: 'Meeting Hermione Granger',
+    date: '1991-09-01',
+    emotions: {
+      fear: 1,
+      anger: 2,
+      sadness: 1,
+      surprise: 4,
+      joy: 5,
+      love: 3
+    }
+  },
+  {
+    event:
+      "Winning the Triwizard Tournament and Witnessing Cedric Diggory's Death",
+    date: '1995-06-24',
+    emotions: {
+      fear: 10,
       anger: 8,
-      sadness: 5,
+      sadness: 10,
       surprise: 7,
-      joy: 4,
+      joy: 1,
       love: 2
     }
   },
   {
-    event: 'Graduation Day',
-    date: '2021-09-01',
+    event: 'Death of Sirius Black',
+    date: '1996-06-18',
     emotions: {
       fear: 8,
-      anger: 6,
-      sadness: 9,
+      anger: 7,
+      sadness: 10,
       surprise: 5,
+      joy: 0,
+      love: 6
+    }
+  },
+  {
+    event: 'Being Sorted into Gryffindor House',
+    date: '1991-09-01',
+    emotions: {
+      fear: 4,
+      anger: 1,
+      sadness: 2,
+      surprise: 6,
       joy: 7,
       love: 3
     }
   },
   {
-    event: 'Moving to a New City',
-    date: '2021-09-01',
+    event: 'Winning the House Cup in His First Year',
+    date: '1992-06-07',
     emotions: {
-      fear: 9,
-      anger: 7,
-      sadness: 8,
-      surprise: 9,
-      joy: 6,
+      fear: 2,
+      anger: 1,
+      sadness: 1,
+      surprise: 5,
+      joy: 10,
       love: 4
     }
   },
   {
-    event: 'Adopting a Pet',
-    date: '2021-09-01',
+    event: 'First Time Playing Quidditch',
+    date: '1991-11-07',
     emotions: {
-      fear: 5,
-      anger: 9,
-      sadness: 7,
-      surprise: 6,
+      fear: 6,
+      anger: 2,
+      sadness: 1,
+      surprise: 7,
       joy: 8,
-      love: 5
+      love: 3
+    }
+  },
+  {
+    event: 'First Kiss with Ginny Weasley',
+    date: '1997-05-02',
+    emotions: {
+      fear: 1,
+      anger: 1,
+      sadness: 1,
+      surprise: 5,
+      joy: 9,
+      love: 10
     }
   }
 ]
-const emotions: EmotionKeys[] = ['fear', 'anger', 'sadness', 'surprise', 'joy', 'love']
+
+const emotions: EmotionKeys[] = [
+  'fear',
+  'anger',
+  'sadness',
+  'surprise',
+  'joy',
+  'love'
+]
 
 const MyResponsiveRadar = ({ data }: { data: EmotionSet[] }) => {
+  const structure = useMemo(() => {
+    const entries = []
+    for (const emotion of emotions) {
+      // Get the emotion values for each memory event
+      const result: { [key: string]: string | number } = {
+        emotion
+      }
+      // Process the values out of the memory history
+      // @ts-ignore
+      data.forEach((memory: Memory) => {
+        result[memory.event] = memory[emotion]
+      })
+      debugger
+      entries.push(result)
+    }
+    return entries
+  }, [data])
 
-    const structure = useMemo(() => {
-        const entries = []
-        for (const emotion of emotions) {
-
-            // Get the emotion values for each memory event
-            const result: { [key: string]: string | number } = {
-                emotion,
+  useEffect(() => {
+    console.log({ structure })
+  }, [structure])
+  return (
+    <ResponsiveRadar
+      data={structure}
+      margin={{ top: 30, right: 30, bottom: 30, left: 30 }}
+      keys={memories.map((entry: Memory) => entry.event)}
+      indexBy="emotion"
+      borderColor={{ from: 'color' }}
+      gridLabelOffset={16}
+      dotSize={5}
+      dotColor={{ theme: 'background' }}
+      dotBorderWidth={2}
+      colors={{
+        scheme: 'spectral'
+      }}
+      blendMode="multiply"
+      motionConfig="wobbly"
+      legends={[
+        {
+          anchor: 'top-left',
+          direction: 'column',
+          translateX: 0,
+          translateY: 0,
+          itemWidth: 20,
+          itemHeight: 20,
+          itemTextColor: '#444',
+          symbolSize: 12,
+          symbolShape: 'circle',
+          effects: [
+            {
+              on: 'hover',
+              style: {
+                itemTextColor: '#000'
+              }
             }
-            // Process the values out of the memory history
-            // @ts-ignore
-            data.forEach((memory: Memory) => {
-                result[memory.event] = memory[emotion]
-            })
-            debugger;
-            entries.push(result)
+          ]
         }
-        return entries;
-    }, [data])
-
-    useEffect(() => {
-        console.log({ structure })
-    }, [structure ])
-    return (
-        <ResponsiveRadar
-            data={structure}
-            keys={memories.map((entry: Memory) => entry.event)}
-            indexBy="emotion"
-            borderColor={{ from: 'color' }}
-            gridLabelOffset={-10}
-            dotSize={5}
-            dotColor={{ theme: 'background' }}
-            dotBorderWidth={2}
-            colors={{
-                scheme: 'spectral'
-            }}
-            blendMode="multiply"
-            motionConfig="wobbly"
-            legends={[
-                {
-                    anchor: 'top-left',
-                    direction: 'column',
-                    translateX: 0,
-                    translateY: 0,
-                    itemWidth: 20,
-                    itemHeight: 20,
-                    itemTextColor: '#444',
-                    symbolSize: 12,
-                    symbolShape: 'circle',
-                    effects: [
-                        {
-                            on: 'hover',
-                            style: {
-                                itemTextColor: '#000'
-                            }
-                        }
-                    ]
-                }
-            ]} />
-    )
+      ]}
+    />
+  )
 }
 
 export default function MemoryDisplay() {
@@ -152,11 +221,13 @@ export default function MemoryDisplay() {
       <h4 className="scroll-m-20 text-xl font-semibold tracking-tight">
         Memories
       </h4>
-      <div className="size-3/4 px-4">
-              <MyResponsiveRadar data={memories.map((entry: Memory) => ({
-                    ...entry.emotions,
-                    event: entry.event
-              }))} />
+      <div className="size-full px-4">
+        <MyResponsiveRadar
+          data={memories.map((entry: Memory) => ({
+            ...entry.emotions,
+            event: entry.event
+          }))}
+        />
       </div>
     </div>
   )
